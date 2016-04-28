05eb48892849467c5d78c981ea23592fd66f7ae6
ShaunTS <sts_0@outlook.com>
2016-04-27 20:43:12 -0400

    Added handler for postgres functions using GenericHandler and GenErrorMap traits

--- app/libs/PSQLHandler.scala
+++ app/libs/PSQLHandler.scala
@@ -3,24 +3,27 @@ package sts.libs.db
 import java.sql.{Connection, SQLException}
 import sts.libs.errors._
 import scalaz.{-\/, \/, \/-}
-import scala.util.{Try}
+import scala.util.{Failure, Success, Try}
 import org.postgresql.util.PSQLException
 import play.api.db.Database
+import sts.libs.{GenericHandler, GenErrorMap}
 
-trait QuietSQL {
+class PSQLErrorMap extends GenErrorMap {
 
-    def sqlErrorMap(e: Throwable)(implicit c: Connection): GenError = e match {
-        case psql: PSQLException if(psql.getSQLState equals "23505") => UniqueKeyViolation.fromDB(psql)(c)
-        case psql: PSQLException => MiscPSQLError(psql)
-        case sql: SQLException => MiscSQLError(sql)
-        case misc => MiscThrown(misc)
-    }
+    val errors = List(
+        UniqueKeyViolation.diagnose,
+        MiscPSQLError.diagnose,
+        MiscSQLError.diagnose,
+        nomatch
+    )
 
-    def apply[T](op: =>T)(implicit c: Connection): \/[GenError, T] =
-        \/.fromTryCatchThrowable[T, Throwable](op)
-            .leftMap(sqlErrorMap(_))
 }
 
-class PSQLHandler(db: Database) extends QuietSQL {
-    
+class PSQLFunctions(val errorMap: PSQLErrorMap) extends GenericHandler[PSQLErrorMap] {
+
+    def apply[T](block: => T)(implicit c: Connection): \/[GenError, T] =
+        Try { block } match {
+            case Success(s) => \/-(s)
+            case Failure(e) => -\/(errorMap(e))
+        }
 }
