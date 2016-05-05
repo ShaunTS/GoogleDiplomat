5b086ec824e9e5568f847e0601d571370522953a
ShaunTS <sts_0@outlook.com>
2016-05-05 14:51:08 -0400

    Added helper methods to Debug.scala to print out Postgres exceptions, misc changes to scrap/test file

--- app/utils/Debug.scala
+++ app/utils/Debug.scala
@@ -33,3 +33,61 @@ package object helpers {
             println(s"\n\t${stack.length - 3} remaining in stack.")
     }
 }
+
+package object psqlHelpers {
+    import java.sql.{Connection, DatabaseMetaData, SQLException}
+    import org.postgresql.util.{PSQLException, PSQLState, ServerErrorMessage}
+
+    def printMeta(implicit c: Connection) {
+        val info = c.getMetaData
+        println("db-url = " +info.getURL)
+    }
+
+    def printServerMsg(msg: ServerErrorMessage) {
+
+        List(
+            "getDetail",
+            "getHint",
+            "getTable",
+            "getConstraint",
+            "getMessage",
+            "getSchema",
+            "getFile",
+            "getWhere",
+            "getColumn",
+            "getInternalQuery",
+            "getSeverity",
+            "getRoutine",
+            "toString"
+        ).zip(List(
+            msg.getDetail,
+            msg.getHint,
+            msg.getTable,
+            msg.getConstraint,
+            msg.getMessage,
+            msg.getSchema,
+            msg.getFile,
+            msg.getWhere,
+            msg.getColumn,
+            msg.getInternalQuery,
+            msg.getSeverity,
+            msg.getRoutine,
+            msg.toString)
+        ).map {
+                case(tag, thing) => "\t" + tag + ": " + thing
+            }.foreach(println)
+    }
+
+    def printExc(e: PSQLException) = {
+        println("\n------------------------------------------------------------")
+
+        List("e", "msg", "state", "code", "next").map("\t" + _)
+            .zip(List(e, e.getMessage, e.getSQLState, e.getErrorCode, e.getNextException))
+            .map {
+                case(tag, thing) => "\t" + tag + ": " + thing + "\n"
+            }.foreach(println)
+        println("------------------------------------------------------------")
+        printServerMsg(e.getServerErrorMessage)
+        println("------------------------------------------------------------\n")
+    }
+}
--- test/experimental/DBRunnerSpec.scala
+++ test/experimental/DBRunnerSpec.scala
@@ -5,123 +5,64 @@ import sts.util.TimeKeeping
 import sts.util.dbrunner._
 import sts.util.debug.helpers._
 
-import anorm.{SQL, SqlParser}
+import anorm.{SQL, SqlParser, NamedParameter, ParameterValue}
 import org.specs2.mutable._
-import play.api.db.Databases
+import play.api.db.{Databases, PooledDatabase, DatabaseConfig}
 import play.api.db.evolutions._
-import play.api.Environment
-import play.api.Play
+import play.api.Configuration
 import play.api.test._
 import play.api.test.Helpers._
 
-import scala.util.{Try, Success, Failure}
-import java.sql.{Connection, DatabaseMetaData, SQLException}
-import org.postgresql.util.{PSQLException, PSQLState, ServerErrorMessage}
+import play.api.inject.guice.GuiceApplicationLoader
+import play.api.{ Application, ApplicationLoader, Environment, Mode }
+
+import play.api.db.Database
 
 
 object DBRunnerSpec extends Specification with TimeKeeping {
 
+    def mkTestDB = Databases(
+        driver = "org.postgresql.Driver",
+        url = "jdbc:postgresql://localhost/google_diplomat_test",
+        config = Map(
+            "user" -> "local_app",
+            "password" -> "stsapp"
+        )
+    )
+
     "DB stuff" >> {
 
-        tag("env")
-        "env config" in {
-            pl("env")(Environment.simple())
+        "app" in {
 
-            success
-        }
+            val config = Configuration.load(Environment.simple())
+            val keys0 = config.getConfig("db.test")
+                .get.keys.toSeq.sorted
 
-        tag("dberr")
-        "DBApi" in {
-
-            val db = Databases(
-                driver = "org.postgresql.Driver",
-                url = "jdbc:postgresql://localhost/google_diplomat_test",
-                config = Map(
-                    "user" -> "local_app",
-                    "password" -> "stsapp"
-                )
-            )
-
-            db.withConnection { implicit c =>
-
-                printMeta
-
-                Try {
-                    val query = SQL("""
-                        INSERT INTO fake_users_groups(user_id, group_id) VALUES ({user_id}, {group_id})
-                    """).on(
-                        "user_id" -> 1,
-                        "group_id" -> 1
-                    )
-                    .executeInsert(SqlParser.scalar[Long].singleOpt)
-
-                    // pl("query")(query)
-                } match {
-                    case Success(s) => ()
-                    case Failure(e: PSQLException) => printExc(e)
-                    case Failure(e) => pl("ERR")(List(e, e.getMessage).mkString("\n"))
-                }
-
-            }
-
-            db.shutdown()
+            val keys1 = Configuration.reference.getConfig("play.db.prototype")
+                .get.keys.toSeq.sorted
+            
+            
+            
+            pl("configs")(keys0.mkString("\n") +"\n\n"+ keys1.mkString("\n"))
+            
             success
         }
     }
 
+        // idleMaxAge: "1 minutes",
+        // idleConnectionTestPeriod: "20 seconds",
+        // connectionTimeout: "20 seconds",
+        // connectionTestStatement: "SELECT 1",
+        // maxConnectionAge: "30 minutes",
+        // bonecp.logStatements: true,
+        // pool: "bonecp"
 
-
-    def printMeta(implicit c: Connection) {
-        val info = c.getMetaData
-        println("db-url = " +info.getURL)
+    def insertEmu(db: Database): Option[Long] = db.withConnection { implicit c =>
+        SQL("""
+            INSERT INTO fake_users(name) VALUES
+            ({name})
+        """).on(
+            "name" -> "emu"
+        ).executeInsert()
     }
-
-    def printServerMsg(msg: ServerErrorMessage) {
-
-        List(
-            "getDetail",
-            "getHint",
-            "getTable",
-            "getConstraint",
-            "getMessage",
-            "getSchema",
-            "getFile",
-            "getWhere",
-            "getColumn",
-            "getInternalQuery",
-            "getSeverity",
-            "getRoutine",
-            "toString"
-        ).zip(List(
-            msg.getDetail,
-            msg.getHint,
-            msg.getTable,
-            msg.getConstraint,
-            msg.getMessage,
-            msg.getSchema,
-            msg.getFile,
-            msg.getWhere,
-            msg.getColumn,
-            msg.getInternalQuery,
-            msg.getSeverity,
-            msg.getRoutine,
-            msg.toString)
-        ).map {
-                case(tag, thing) => "\t" + tag + ": " + thing
-            }.foreach(println)
-    }
-
-    def printExc(e: PSQLException) = {
-        println("\n------------------------------------------------------------")
-
-        List("e", "msg", "state", "code", "next").map("\t" + _)
-            .zip(List(e, e.getMessage, e.getSQLState, e.getErrorCode, e.getNextException))
-            .map {
-                case(tag, thing) => "\t" + tag + ": " + thing + "\n"
-            }.foreach(println)
-        println("------------------------------------------------------------")
-        printServerMsg(e.getServerErrorMessage)
-        println("------------------------------------------------------------\n")
-    }
-
 }
