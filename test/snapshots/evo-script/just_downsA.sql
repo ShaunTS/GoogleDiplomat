d1911d5fff892343d447fca7f2a32fd74bc3c859
ShaunTS <sts_0@outlook.com>
2016-05-10 18:52:15 -0400

    Removed unused snapshots

--- test/snapshots/evo-script/just_downsA.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-
-# --- !Downs
-DROP TABLE IF EXISTS "nearby_search_requests";
-DROP TABLE IF EXISTS "test_place_types";
-DROP TABLE IF EXISTS "test_place_type_use_types";
-DROP TABLE IF EXISTS "test_api_keys";
--- test/snapshots/evo-script/just_downsB.sql
+++
@@ -1,6 +0,0 @@
-
-# --- !Downs
-DROP TABLE IF EXISTS "nearby_search_requests";
-DROP TABLE IF EXISTS "test_place_types";
-DROP TABLE IF EXISTS "test_place_type_use_types";
-DROP TABLE IF EXISTS "test_api_keys";
