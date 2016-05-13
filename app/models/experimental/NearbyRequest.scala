b64324a314a0abae41d78e457b1fadb40d268bff
ShaunTS <sts_0@outlook.com>
2016-05-13 01:04:40 -0400

    Updated NearbyRequest to use PSQLHandler for database interactions, implemented row parser to derive the model from query results, added single fetch method using parser

--- app/models/experimental/NearbyRequest.scala
+++ app/models/experimental/NearbyRequest.scala
@@ -2,14 +2,83 @@ package sts.diplomat.models
 
 import anorm._
 import anorm.SqlParser._
-import java.sql.Connection
-import play.api.db.DB
-import play.api.libs.json.{Json, Writes, Reads}
-import play.api.Play.current
-import scala.util.{Try, Success, Failure}
+import org.postgresql.util.PGobject
+import play.api.db.Database
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{__, Json, Writes, Reads}
 import scalaz.{\/}
+import sts.libs.db.PostgresHelpers._
+import sts.libs.db.{PostgresOps, SchemaInfo}
+import sts.libs.errors.GenError
 import sts.libs.json.{JsParam, JsParamList, JsParamLists}
 
+case class NearbyRequest(
+    id: Option[Long],
+    loc: Coords,
+    radius: Int,
+    pageToken: Option[String] = None,
+    extraParams: NearbyParams = NearbyParams.default
+) extends Model[NearbyRequest] {
+
+    def withId(id: Long): NearbyRequest = this.copy(id = Some(id))
+
+    def withParams(params: JsParam[_] * ): NearbyRequest = this.copy(
+        extraParams = this.extraParams.withValues(params: _ *)
+    )
+}
+
+object NearbyRequest extends PostgresOps {
+
+    val schema = SchemaInfo(
+        tableName = "nearby_search_requests",
+        entityName = "NearbyRequest"
+    )
+
+    val baseUrl: String = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+    def empty: NearbyRequest = NearbyRequest(None, Coords.empty, 0, None, NearbyParams.default)
+
+    val parser: RowParser[NearbyRequest] = {
+        get[Option[Long]]("nearby_search_requests.id") ~
+        Coords.parser("nearby_search_requests") ~
+        int("nearby_search_requests.radius") ~
+        get[Option[String]]("nearby_search_requests.pg_token") ~
+        get[Option[PGobject]]("nearby_search_requests.params").map(_.map(_.getValue)) map {
+            case id~coords~radius~token~paramsJSON =>
+                NearbyRequest(id, coords, radius, token, NearbyParams.parse(paramsJSON))
+        }
+    }
+
+    def create(req: NearbyRequest)(implicit db: Database): \/[GenError, NearbyRequest] = PSQLHandler.create { implicit c =>
+
+        SQL("""
+            INSERT INTO nearby_search_requests(lat, lng, radius, pg_token, params) VALUES
+            ({lat}, {lng}, {radius}, {token}, toJSON({params}))
+        """).on(
+            "lat" -> req.loc.lat,
+            "lng" -> req.loc.lng,
+            "radius" -> req.radius,
+            "token" -> req.pageToken,
+            "params" -> req.extraParams.stringOpt
+        ).executeInsert()
+            .map(req.withId(_))
+    }
+
+    def find(id: Long)(implicit db: Database): \/[GenError, NearbyRequest] = PSQLHandler.find { implicit c =>
+        SQL("SELECT * FROM nearby_search_requests WHERE id = {id}").on("id" -> id)
+        .as(parser.singleOpt)
+    }
+
+    implicit val writes: Writes[NearbyRequest] = Json.writes[NearbyRequest]
+
+    implicit val reads: Reads[NearbyRequest] = (
+        (__ \ "id").readNullable[Long] and
+        (__ \ "loc").read[Coords] and
+        (__ \ "radius").read[Int] and
+        (__ \ "pageToken").readNullable[String] and
+        (__ \ "extraParams").read[NearbyParams]
+    )(NearbyRequest.apply _)
+}
 
 
 case class NearbyParams(params: Seq[JsParam[_]]) extends JsParamList[NearbyParams] {
@@ -29,51 +98,4 @@ object NearbyParams extends JsParamLists[NearbyParams] {
         JsParam.empty[String]("type"),
         JsParam.empty[List[String]]("types")
     )
-}
-
-case class NearbyRequest(
-    id: Option[Long],
-    loc: Coords,
-    radius: Int,
-    pageToken: Option[String] = None,
-    extraParams: NearbyParams = NearbyParams.default
-) extends Model[NearbyRequest] {
-
-    def withId(id: Long): NearbyRequest = this.copy(id = Some(id))
-
-    def withParams(params: JsParam[_] * ): NearbyRequest = this.copy(
-        extraParams = this.extraParams.withValues(params: _ *)
-    )
-}
-
-object NearbyRequest {
-
-    val baseUrl: String = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-
-    // implicit val writes: Writes[NearbyRequest] = (
-    //     (__ \ "id").writeNullable[Long] and
-    //     (__ \ "loc").write[Coords] and
-    //     (__ \ "radius").write[Int] and
-    //     (__ \ "pageToken").writeNullable[String]
-    // )
-
-    def empty: NearbyRequest = NearbyRequest(None, Coords.empty, 0, None, NearbyParams.default)
-
-    def create(req: NearbyRequest): Option[NearbyRequest] = {
-
-        DB.withTransaction { implicit c =>
-
-            SQL("""
-                INSERT INTO nearby_search_requests(lat, lng, radius, params) VALUES
-                ({lat}, {lng}, {radius}, jsonOrNull({params}))
-            """).on(
-                "lat" -> req.loc.lat,
-                "lng" -> req.loc.lng,
-                "radius" -> req.radius,
-                "params" -> req.extraParams.stringOpt
-            ).executeInsert()
-                .map(req.withId(_))
-        }
-    }
-
 }
