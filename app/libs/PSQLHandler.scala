58f2cb831a2ea980e33a67f493551dee59d31c8f
ShaunTS <sts_0@outlook.com>
2016-05-13 01:00:28 -0400

    Implemented 'PSQLHandler' functions which pass implicit Database references to eventually create and use a java.sql.Connection reference, mapping exceptions to project error classes and optional results to non-error result classes

--- app/libs/PSQLHandler.scala
+++ app/libs/PSQLHandler.scala
@@ -1,12 +1,98 @@
 package sts.libs.db
 
-import java.sql.{Connection, SQLException}
-import sts.libs.errors._
-import scalaz.{-\/, \/, \/-}
-import scala.util.{Failure, Success, Try}
-import org.postgresql.util.PSQLException
+import java.sql.Connection
 import play.api.db.Database
+import scala.util.{Failure, Success, Try}
+import scalaz.{-\/, \/, \/-}
+import sts.libs.errors._
 import sts.libs.{GenericHandler, GenErrorMap}
+
+/**
+ *  @todo Add comments
+ */
+
+
+class PSQLFunctions(
+    val schema: SchemaInfo,
+    val errorMap: PSQLErrorMap
+) extends GenericHandler[PSQLErrorMap] {
+
+
+
+    def transact[A](task: Connection => \/[GenError, A])(implicit db: Database): \/[GenError, A] =
+        db.withConnection(autocommit = false) { implicit connection =>
+            val result = task(connection)
+
+            result fold (
+                error => connection.rollback(),
+                success => connection.commit()
+            )
+
+            result
+        }
+
+    def withConnection[A](task: Connection => A)(implicit db: Database): \/[GenError, A] =
+        db.withConnection { implicit c => apply(task(c)) }
+
+    def optional[A](task: Connection => Option[A], noResult: SQLNoResult)(implicit db: Database): \/[GenError, A] =
+        db.withConnection { implicit c => optional(task(c), noResult.withURL(db.url) ) }
+
+
+
+
+    def withTransaction[A](task: Connection => A)(implicit db: Database): \/[GenError, A] =
+        transact(implicit c => apply(task(c)))
+
+    def transactOption[A](task: Connection => Option[A], noResult: SQLNoResult)(implicit db: Database): \/[GenError, A] =
+        transact(implicit c => optional(task(c), noResult))
+
+
+    def find[A](task: Connection => Option[A])(implicit db: Database): \/[GenError, A] = optional(task, FailedRead)
+
+    def create[A](task: Connection => Option[A])(implicit db: Database): \/[GenError, A] = optional(task, FailedCreate)
+
+    def update[A](task: Connection => Option[A])(implicit db: Database): \/[GenError, A] = optional(task, FailedUpdate)
+
+
+
+
+    def apply[A](task: => A)(implicit c: Connection): \/[GenError, A] =
+    Try { task } match {
+        case Success(s) => \/-(s)
+        case Failure(e) => -\/(errorMap(e))
+    }
+
+    def optional[A](task: => Option[A], noResult: SQLNoResult)(implicit c: Connection): \/[GenError, A] =
+    apply(task)(c) flatMap {
+        case Some(result) => \/-(result)
+        case None => -\/( noResult.withURL(c.getMetaData.getURL) )
+    }
+
+    def create[A](task: => Option[A])(implicit c: Connection): \/[GenError, A] = optional(task, FailedCreate)
+
+    def update[A](task: => Option[A])(implicit c: Connection): \/[GenError, A] = optional(task, FailedUpdate)
+
+
+
+    def FailedAction: SQLNoResult = (schema makeError "Failed action from")
+
+    def FailedRead: SQLNoResult = (schema makeError "Could not find requested")
+
+    def FailedCreate: SQLNoResult = (schema makeError "Failed to create")
+
+    def FailedUpdate: SQLNoResult = (schema makeError "Failed to update")
+}
+
+
+trait PostgresOps {
+
+    def errorMap: PSQLErrorMap = new PSQLErrorMap()
+
+    def schema: SchemaInfo
+
+    lazy val PSQLHandler: PSQLFunctions = new PSQLFunctions(schema, errorMap)
+}
+
 
 class PSQLErrorMap extends GenErrorMap {
 
@@ -19,11 +105,12 @@ class PSQLErrorMap extends GenErrorMap {
 
 }
 
-class PSQLFunctions(val errorMap: PSQLErrorMap) extends GenericHandler[PSQLErrorMap] {
+case class SchemaInfo(tableName: String, entityName: String) {
 
-    def apply[T](block: => T)(implicit c: Connection): \/[GenError, T] =
-        Try { block } match {
-            case Success(s) => \/-(s)
-            case Failure(e) => -\/(errorMap(e))
-        }
+
+    def makeError(action: String) = SQLNoResult(
+        table = Some(tableName),
+        dbURL = None,
+        message = s"$action `$entityName` in relation `$tableName`"
+    )
 }
---
+++ app/libs/PostgresHelpers.scala
@@ -0,0 +1,30 @@
+
+package sts.libs.db
+
+import anorm._
+import org.postgresql.util.PGobject
+import sts.util.debug.helpers._
+
+
+object PostgresHelpers {
+
+    def className(that: Any): String =
+    if (that == null) "<null>" else that.getClass.getName
+
+    /**
+     *  @todo clean up printlns, add option/null/json handling
+     */
+    implicit val columnToPGobject: Column[PGobject] =
+        Column.nonNull[PGobject] { (value, meta) =>
+            val MetaDataItem(qualified, nullable, clazz) = meta
+
+            value match {
+                case obj: PGobject => pl("type")(obj.getType)
+                pl("value")(obj.getValue)
+            }
+            value match {
+                case obj: PGobject => Right(obj)
+                case _ => Left(TypeDoesNotMatch(s"Cannot convert $value: ${className(value)} to String for column $qualified"))
+            }
+        }
+}
